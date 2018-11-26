9262f282f4fcfcf339ade69fd92bcf878f6b3a35
Jose Garcia <josegarciamanez@gmail.com>
2018-11-26 16:35:11 +0100

    primera animacion

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+    text-decoration: none;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Jose García Máñez</h2>
+    <HelloWorld msg="Vue.js & Anime.js"/>
+    <button @click="animacion">Anime</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import anime from '../../node_modules/animejs'
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  methods: {
+    animacion() {
+        anime({
+            targets: "h2",
+            translateY: [
+                { value: 250, duration: 1500 },
+                { value: 0, duration: 1500 }
+            ],
+            translateX: [
+                { value: 50, duration: 1500 },
+                { value: 0, duration: 1500 }
+            ],
+            rotate: "1turn",
+            // backgroundColor: "#000",
+            duration: 3000,
+            loop: false
+        });
+    }
   }
 };
 </script>
